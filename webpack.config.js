e4a614bfbbb776d95acad09f0d2b6c65fee156aa
Lihua <41542302+ytop@users.noreply.github.com>
2025-06-18 13:50:46 +0000

    replit compatible
    replit dev run

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,56 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { VueLoaderPlugin } = require('vue-loader');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: [".js", ".vue", ".json"],
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      'vue$': 'vue/dist/vue.esm.js'
-    }
+      "@": path.resolve(__dirname, "src"),
+      vue$: "vue/dist/vue.esm.js",
+    },
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: "vue-loader",
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        loader: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
+          name: "[name].[ext]?[hash]",
+        },
+      },
+    ],
   },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html'
-    })
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
   ],
   devServer: {
     port: 8081,
     hot: true,
-    historyApiFallback: true
-  }
+    allowedHosts: "all",
+    historyApiFallback: true,
+  },
 };
