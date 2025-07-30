425d86e384aa8bf1fe23656dd28358bf0854d559
Steven Li <lishuyustevenli@gmail.com>
2025-07-30 14:53:19 -0400

    fix linter

--- src/views/AdminManagement.vue
+++ src/views/AdminManagement.vue
@@ -216,7 +216,7 @@ export default {
     AdminSystemOverview,
     AdminUserPermissionsDialog,
     AdminUserDetailsDialog,
-    AdminKRIPermissionsDialog,
+    // AdminKRIPermissionsDialog,
     AdminBulkPermissionTemplateDialog
   },
   
